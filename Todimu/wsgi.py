5c0ce2d8b95f67d42066491072f0088baf0fa323
Oluwatodimu <todimuisewon@gmail.com>
2022-05-30 19:58:08 +0100

    Created venv and project

--- Todimu/wsgi.py
+++ Todimu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OluwatodimuIsewon project.
+WSGI config for Todimu project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OluwatodimuIsewon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Todimu.settings')
 
 application = get_wsgi_application()
